323f31c23b889403050d9e645c7a4e0cdafc961a
Justin Love <git@JustinLove.name>
2014-03-02 20:07:23 -0600

    working dialog

--- root/ui/mods/pa_mod_hotfix/pa_mod_hotfix.js
+++ root/ui/mods/pa_mod_hotfix/pa_mod_hotfix.js
@@ -1,9 +1,30 @@
 (function() {
+  var setupDialog = function() {
+    $(".{%= name %}_dialog").dialog({
+      dialogClass: "no-close",
+      draggable: false,
+      resizable: false,
+      height: 400,
+      width: 600,
+      modal: true,
+      autoOpen: model.buildVersion() != '{%= build %}',
+      buttons: {
+          "EXIT": function () {
+              model.exit();
+          },
+          "LATER": function () {
+              $(this).dialog("close");
+          }
+      }
+    });
+  }
+
   //load html dynamically
   loadTemplate = function (element, url, model) {
     element.load(url, function () {
       console.log("Loading html " + url);
       ko.applyBindings(model, element.get(0));
+      setupDialog()
     });
   };
 
